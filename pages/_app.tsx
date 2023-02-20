8ae1433a4e82343b70c76c66afe21e974047832d
gabriel.anacik <gabriel.anacik@rad4m.com>
2023-02-20 18:16:10 +0100

    haha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from 'next-auth/react';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
+    // @ts-ignore: Unreachable code error
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
